49c1fc8170a647b056fca197289cddfae590abb4
jerry <1347401764@qq.com>
2026-01-04 23:34:17 +0800

    fix: 替换 WebRTC 依赖为 io.getstream:stream-webrtc-android:1.3.10 (Fix NoClassDefFoundError)

--- android-remote-control/app/build.gradle.kts
+++ android-remote-control/app/build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.8.2")
 
     // WebRTC
-    implementation("com.infobip:google-webrtc:1.0.45036")
+    implementation("io.getstream:stream-webrtc-android:1.3.10")
 
     // WebSocket (OkHttp)
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
